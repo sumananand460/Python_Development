500b847882a5645651408e63e59bd59ae3262427
Suman Anand <sumananand460@gmail.com>
2026-05-11 19:08:26 +0530

    Added new file

---
+++ Module2/Practise.py
@@ -0,0 +1,11 @@
+# Control Statements:
+# => Used to make decisions based on the statements
+
+# "if" statment
+# => Works when if the condition is true
+
+age = int(input("Enter your age: "))
+if age <= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
